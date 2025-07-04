623e49b91c8c2647ff93332a7a9854ab4d728577
azlifedosemon <stanislaus.dosemon@allianz.co.id>
2025-07-04 13:57:23 +0700

    IRCS2-0.0.1.1 [rename folder]

---
+++ IRCS2-devbuild-0.0.1.0/IRCS2-input.py
@@ -0,0 +1,19 @@
+# Input Script
+
+# 1) copy past the file path, make sure the 
+#    files are in the same folder with this python program
+
+# DV AZTRAD (CSV)
+DV_AZTRAD_path = "D:\1. IRCS Automation\Control 2 DEV\IRCS\Control 2\DV_AZTRAD_Stat.csv"
+
+# DV AZUL (CSV)
+DV_AZUL_path = "D:\1. IRCS Automation\Control 2 DEV\IRCS\Control 2\DV_AZUL_Stat.csv"
+
+# IT AZTRAD (CSV)
+IT_AZTRAD_path = "D:\1. IRCS Automation\Control 2 DEV\IRCS\Control 2\IT_AZTRAD_FULL_Stat.csv"
+
+# IT AZUL (CSV)
+IT_AZUL_path = "D:\1. IRCS Automation\Control 2 DEV\IRCS\Control 2\IT_AZUL_FULL_Stat.csv"
+
+#CODE LIBRARY
+CODE_LIBRARY_path = "D:\1. IRCS Automation\Control 2 DEV\IRCS\Control 2\CODE LIBRARY.xlsx"
