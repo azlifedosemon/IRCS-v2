b625eb08a45263b09a60af44beac15b3c0eaac76
Christo-Christo <christo@allianz.co.id>
2025-08-01 15:06:19 +0700

    ...

--- test.py
+++ test.py
@@ -1,37 +1,18 @@
+❌ File not found: d:\Run Control 3\IRCS3_build\rafm_run13.pkl
+❌ File not found: d:\Run Control 3\IRCS3_build\rafm_run4.pkl
+❌ File not found: d:\Run Control 3\IRCS3_build\rafm_run23.pkl
+❌ Error in RAFM worker for run run13: Command '['C:\\Users\\christo\\AppData\\Local\\Microsoft\\WindowsApps\\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\\python.exe', 'd:\\Run Control 3\\IRCS3_build\\rafm_worker.py', 'D:\\Run Control 3\\Source\\Trad\\Data_Extraction_run13TRAD_Con.xlsx', 'd:\\Run Control 3\\IRCS3_build\\rafm_run13.pkl']' returned non-zero exit status 1.
+❌ Error in RAFM worker for run run4: Command '['C:\\Users\\christo\\AppData\\Local\\Microsoft\\WindowsApps\\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\\python.exe', 'd:\\Run Control 3\\IRCS3_build\\rafm_worker.py', 'D:\\Run Control 3\\Source\\Trad\\Data_Extraction_run4TRAD_Con.xlsx', 'd:\\Run Control 3\\IRCS3_build\\rafm_run4.pkl']' returned non-zero exit status 1.
+❌ Error in RAFM worker for run run23: Command '['C:\\Users\\christo\\AppData\\Local\\Microsoft\\WindowsApps\\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\\python.exe', 'd:\\Run Control 3\\IRCS3_build\\rafm_worker.py', 'D:\\Run Control 3\\Source\\Trad\\Data_Extraction_run23TRAD_Con.xlsx', 'd:\\Run Control 3\\IRCS3_build\\rafm_run23.pkl']' returned non-zero exit status 1.
+❌ File not found: d:\Run Control 3\IRCS3_build\rafm_run142.pkl
+❌ Error in RAFM worker for run run142: Command '['C:\\Users\\christo\\AppData\\Local\\Microsoft\\WindowsApps\\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\\python.exe', 'd:\\Run Control 3\\IRCS3_build\\rafm_worker.py', 'D:\\Run Control 3\\Source\\Trad\\Data_Extraction_run142TRAD_Con.xlsx', 'd:\\Run Control 3\\IRCS3_build\\rafm_run142.pkl']' returned non-zero exit status 1.
 Traceback (most recent call last):
   File "d:\Run Control 3\IRCS3_build\IRCS3_program.py", line 5, in <module>
     import trad_processing as trad
-  File "d:\Run Control 3\IRCS3_build\trad_processing.py", line 245, in <module>
-    dv_cache = load_dv_excels(tradfilter)
-               ^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "d:\Run Control 3\IRCS3_build\trad_processing.py", line 147, in load_dv_excels
-    df = pd.read_excel(path, engine = 'openpyxl')
-         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "C:\Users\christo\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\site-packages\pandas\io\excel\_base.py", line 478, in read_excel
-    io = ExcelFile(io, storage_options=storage_options, engine=engine)
-         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "C:\Users\christo\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\site-packages\pandas\io\excel\_base.py", line 1513, in __init__
-    self._reader = self._engines[engine](self._io, storage_options=storage_options)
-                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "C:\Users\christo\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\site-packages\pandas\io\excel\_openpyxl.py", line 549, in __init__
-    super().__init__(filepath_or_buffer, storage_options=storage_options)
-  File "C:\Users\christo\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\site-packages\pandas\io\excel\_base.py", line 540, in __init__
-    self.book = self.load_workbook(self.handles.handle)
-                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "C:\Users\christo\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\site-packages\pandas\io\excel\_openpyxl.py", line 560, in load_workbook
-    return load_workbook(
-           ^^^^^^^^^^^^^^
-  File "C:\Users\christo\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\site-packages\openpyxl\reader\excel.py", line 344, in load_workbook
-    reader = ExcelReader(filename, read_only, keep_vba,
-             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "C:\Users\christo\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\site-packages\openpyxl\reader\excel.py", line 123, in __init__
-    self.archive = _validate_archive(fn)
-                   ^^^^^^^^^^^^^^^^^^^^^
-  File "C:\Users\christo\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\site-packages\openpyxl\reader\excel.py", line 95, in _validate_archive
-    archive = ZipFile(filename, 'r')
-              ^^^^^^^^^^^^^^^^^^^^^^
-  File "C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.11_3.11.2544.0_x64__qbz5n2kfra8p0\Lib\zipfile.py", line 1312, in __init__  
-    self._RealGetContents()
-  File "C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.11_3.11.2544.0_x64__qbz5n2kfra8p0\Lib\zipfile.py", line 1379, in _RealGetContents
-    raise BadZipFile("File is not a zip file")
-zipfile.BadZipFile: File is not a zip file
+  File "d:\Run Control 3\IRCS3_build\trad_processing.py", line 274, in <module>
+    rafm_runs = build_rafm_subprocess(tradfilter)
+                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "d:\Run Control 3\IRCS3_build\trad_processing.py", line 212, in build_rafm_subprocess
+    run, df = fut.result()
+    ^^^^^^^
+TypeError: cannot unpack non-iterable NoneType object
