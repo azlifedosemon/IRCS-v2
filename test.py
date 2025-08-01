e7c1f0d7a275e8e8d1e3c8cab402f56e64a44fab
Christo-Christo <christo@allianz.co.id>
2025-08-01 16:11:02 +0700

    ...

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
-ERROR CLASH in FILTER_TRAD:
-• run4: channel → nan; currency → nan; cohort → nan; period → nan
-• run13: channel → nan; currency → nan; period → nan
-• run23: channel → nan; currency → nan; period → nan
-• run142: channel → nan; currency → nan; cohort → nan; period → nan
+PS D:\Run Control 3> & C:\Users\christo\AppData\Local\Microsoft\WindowsApps\python3.11.exe "d:/Run Control 3/IRCS3_build/IRCS3_program.py"
+❌ Error in RAFM subprocess for run142: expected str, bytes or os.PathLike object, not NoneType
+❌ Error in RAFM subprocess for run142: expected str, bytes or os.PathLike object, not NoneType
+❌ Error in RAFM subprocess for run142: expected str, bytes or os.PathLike object, not NoneType
+❌ Error in RAFM subprocess for run142: expected str, bytes or os.PathLike object, not NoneType
+Traceback (most recent call last):
+  File "d:\Run Control 3\IRCS3_build\IRCS3_program.py", line 5, in <module>
+    import trad_processing as trad
+  File "d:\Run Control 3\IRCS3_build\trad_processing.py", line 303, in <module>
+    rafm_df = rafm_runs[run_name]
+              ~~~~~~~~~^^^^^^^^^^
+KeyError: 'run4'
